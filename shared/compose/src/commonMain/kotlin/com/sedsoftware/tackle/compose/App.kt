c3ad4acd65a470dc9c3e16a074baea6e6e03431b
Sergey V <djkovrik@gmail.com>
2024-03-30 16:22:51 +0300

    Custom app theme

--- shared/compose/src/commonMain/kotlin/com/sedsoftware/tackle/compose/App.kt
+++ shared/compose/src/commonMain/kotlin/com/sedsoftware/tackle/compose/App.kt
@@ -2,17 +2,17 @@ package com.sedsoftware.tackle.compose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.sedsoftware.tackle.compose.theme.TackleTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    TackleTheme {
         Column(
             modifier = Modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
