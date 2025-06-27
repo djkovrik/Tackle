59f8324b93f60f6bbcc2872c3fa41ac8bbad03c9
DJ_Kovrik <djkovrik@gmail.com>
2025-06-27 18:20:10 +0300

    Display gallery seleced item indicator

--- shared/compose/src/commonMain/kotlin/com/sedsoftware/tackle/compose/ui/status/content/StatusAttachmentsImageList.kt
+++ shared/compose/src/commonMain/kotlin/com/sedsoftware/tackle/compose/ui/status/content/StatusAttachmentsImageList.kt
@@ -1,6 +1,10 @@
 package com.sedsoftware.tackle.compose.ui.status.content
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -10,15 +14,18 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -26,6 +33,9 @@ import com.sedsoftware.tackle.compose.model.TackleImageParams
 import com.sedsoftware.tackle.compose.widget.TackleImage
 import com.sedsoftware.tackle.domain.model.MediaAttachment
 import com.sedsoftware.tackle.utils.extension.orZero
+import org.jetbrains.compose.resources.painterResource
+import tackle.shared.compose.generated.resources.Res
+import tackle.shared.compose.generated.resources.attachment_done
 
 @Composable
 internal fun StatusAttachmentsImageList(
@@ -48,7 +58,7 @@ internal fun StatusAttachmentsImageList(
             modifier = Modifier.fillMaxWidth(),
         )
 
-        AnimatedVisibility(visible = attachments.size > 1,) {
+        AnimatedVisibility(visible = attachments.size > 1) {
             LazyRow(modifier = Modifier.padding(all = 4.dp)) {
                 itemsIndexed(
                     items = attachments,
@@ -83,12 +93,13 @@ private fun StatusAttachmentGalleryItem(
     modifier: Modifier = Modifier,
 ) {
     Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
             .clip(shape = MaterialTheme.shapes.extraSmall)
             .border(
                 width = 1.dp,
                 color = if (selected) {
-                    MaterialTheme.colorScheme.outline.copy(alpha = 0.5f)
+                    MaterialTheme.colorScheme.primary
                 } else {
                     MaterialTheme.colorScheme.surface.copy(alpha = 0.1f)
                 },
@@ -104,5 +115,18 @@ private fun StatusAttachmentGalleryItem(
                 .height(height = 64.dp)
                 .aspectRatio(ratio = params?.ratio ?: 1f)
         )
+
+        AnimatedVisibility(
+            visible = selected,
+            enter = scaleIn() + fadeIn(),
+            exit = scaleOut() + fadeOut(),
+        ) {
+            Icon(
+                painter = painterResource(resource = Res.drawable.attachment_done),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.surface,
+                modifier = Modifier.size(size = 24.dp)
+            )
+        }
     }
 }
