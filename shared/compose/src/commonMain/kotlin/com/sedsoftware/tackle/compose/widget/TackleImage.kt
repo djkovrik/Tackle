9322e4a52e63277fd0c0fb0e0f3aa1a21438d87b
DJ_Kovrik <djkovrik@gmail.com>
2025-07-08 16:42:05 +0300

    Switch to content for TackleImage

--- shared/compose/src/commonMain/kotlin/com/sedsoftware/tackle/compose/widget/TackleImage.kt
+++ shared/compose/src/commonMain/kotlin/com/sedsoftware/tackle/compose/widget/TackleImage.kt
@@ -12,7 +12,10 @@ import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
 import androidx.compose.ui.layout.ContentScale
+import com.github.panpf.sketch.AsyncImageState
+import com.github.panpf.sketch.PainterState
 import com.github.panpf.sketch.SubcomposeAsyncImage
+import com.github.panpf.sketch.rememberAsyncImageState
 import com.sedsoftware.tackle.compose.model.TackleImageParams
 
 @Composable
@@ -28,30 +31,38 @@ internal fun TackleImage(
     filterQuality: FilterQuality = DefaultFilterQuality,
 ) {
     val imageRatio: Float = remember { imageParams.takeIf { it != null }?.ratio ?: 1f }
+    val state: AsyncImageState = rememberAsyncImageState()
 
     SubcomposeAsyncImage(
         uri = imageUrl,
+        state = state,
         contentDescription = contentDescription,
         modifier = modifier.aspectRatio(ratio = imageRatio),
-        loading = {
-            TackleImageLoading(
-                blurhash = imageParams?.blurhash.orEmpty(),
-                modifier = modifier.aspectRatio(ratio = imageRatio),
-            )
-        },
-        error = {
-            TackleImageError(
-                modifier = modifier.aspectRatio(ratio = imageRatio),
-            )
-        },
-        success = {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                modifier = modifier
-                    .aspectRatio(ratio = imageRatio)
-                    .fillMaxSize(),
-            )
+        content = {
+            when (state.painterState) {
+                is PainterState.Loading -> {
+                    TackleImageLoading(
+                        blurhash = imageParams?.blurhash.orEmpty(),
+                        modifier = modifier.aspectRatio(ratio = imageRatio),
+                    )
+                }
+
+                is PainterState.Error -> {
+                    TackleImageError(
+                        modifier = modifier.aspectRatio(ratio = imageRatio),
+                    )
+                }
+
+                else -> {
+                    Image(
+                        painter = painter,
+                        contentDescription = contentDescription,
+                        modifier = modifier
+                            .aspectRatio(ratio = imageRatio)
+                            .fillMaxSize(),
+                    )
+                }
+            }
         },
         alignment = alignment,
         alpha = alpha,
